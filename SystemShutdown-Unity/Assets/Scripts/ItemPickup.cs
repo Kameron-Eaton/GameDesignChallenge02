6f089ee86a84f77a505f51bbaa698463bcf5324e
Kameron Eaton <kwe8364@utulsa.edu>
2022-04-09 20:20:15 -0500

    Scaled back inventory system
    Inventory system uses game objects instead of scriptable objects

---
+++ SystemShutdown-Unity/Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,48 @@
+/* Created by: Kameron Eaton
+ * Date Created: April 9, 2022
+ * 
+ * Last Edited by: NA
+ * Last Edited: April 9, 2022
+ * 
+ * Description: Scripted event for picking up an item and adding it to inventory
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public GameObject activateOB;
+    bool inReach;
+
+    private void Start()
+    {
+        inReach = false;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        GameObject otherGo = other.gameObject;
+        Debug.Log("Collided with " + otherGo.name);
+        if(otherGo.tag == "Player")
+        {
+                inReach = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        GameObject otherGo = other.gameObject;
+        if (otherGo.tag == "Player")
+            inReach = false;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && inReach)
+        {
+            activateOB.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
