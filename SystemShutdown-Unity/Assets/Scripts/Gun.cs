cfcac3ed770376758988600e716cdcf7d7cd81a4
stephensalah <stephen@salahfamily.org>
2022-04-23 12:56:15 -0500

    MAde working shoot and destroy mechanism

---
+++ SystemShutdown-Unity/Assets/Scripts/Gun.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    
+    public float range = 100f;
+
+    public Camera fpsCam;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")){
+            Shoot();
+        }
+    }
+    void Shoot(){
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)){
+            Debug.Log(hit.transform.name);
+            Destroy target=hit.transform.GetComponent<Destroy>();
+            if (target !=null){
+                target.Die();
+            }
+
+            
+        }
+
+    }
+}
